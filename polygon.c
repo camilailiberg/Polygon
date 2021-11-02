2dd496eec0fe04d7cfabb4343032c1cb608fa39e
camila <camilailiberg@gmail.com>
2021-11-02 14:02:31 -0400

    reads user input and goes to where it has to handle what was asked. Now i will work on handling functions

--- polygon.c
+++ polygon.c
@@ -10,48 +10,56 @@ void main(int argc, char** argv) {
     //? polygonList is a list of Polygon pointer to list of pointers to Polygons
     Polygon** polygonList = (Polygon**)malloc((numberOfPolygons) * sizeof(Polygon));
 
-    // Polygon listOfPlygons[ numberOfPolygons ] ; //? list of polygons
-
-    // char command[MAX_COMMAND_TOKEN_LENGTH];
+    char command[MAX_COMMAND_TOKEN_LENGTH];
     char lastCharacter, *verticesLine;
 
-    char command[100];
-    char tokentemp[20];
+
     char firsttoken[20];
     char rest[100];
-    char *token [3];
-    int polygonindex;
 
     // strcpy(command, "add 3 4 1 2 3 4 5 6 7 8 9 1\n");
+    
+    //? letting the user know the program started
+    printf("Start of the program: Valid commands are add, summary, shift, turn, and quit\n\n");
     while (1) {//infinite while loop...
+        //?getting the user input and storing it in command.
+        //?  Here, [] is the scanset character. ^\n tells to take input until newline doesn’t get encountered. Then, with this %*c, it reads newline character and here used * indicates that this newline character is discarded.
+        scanf("%[^\n]%*c", command);
+        printf("command = %s\n", command); //TODO: DELETE
+
         sscanf(command, "%s %[^\n]\n", firsttoken, rest);
         if(!strcmp(firsttoken, "quit")){
-            printf("firsttoken is %s\n", firsttoken);
-            printf("rest is %s\n", rest);
+            printf("rest is %s\n", rest); //TODO: DELETE
+            printf("firsttoken is %s\n", firsttoken); //TODO: DELETE
+            break ;
         }
         if(!strcmp(firsttoken, "summary")){
-            printf("firsttoken is %s\n", firsttoken);
-            printf("rest is %s\n", rest);
-        }
-        if(!strcmp(firsttoken, "shift")){
-            printf("firsttoken is %s\n", firsttoken);
-            printf("rest is %s\n", rest);
-        }
-        if(!strcmp(firsttoken, "turn")){
-            printf("firsttoken is %s\n", firsttoken);
-            printf("rest is %s\n", rest);
+            printf("firsttoken is %s\n", firsttoken); //TODO: DELETE
+            printf("rest is %s\n", rest); //TODO: DELETE
+
+            //* ADD CODE FOR HANDLING ADD
         }
         if(!strcmp(firsttoken, "add")){
-            // printf("rest is %s\n", rest);
-            // int n = strlen(rest), x, y;
-            // rest[n] = '\n';
-            // rest[n+1] = '0';
-            // if(sscanf(rest, "%d %d %[^\n]\n", &x, &y, rest) != 3)
-            //     printf("Error");
-            // else
-            //     printf("coordinate of the first point is (%d, %d), and the rest is %s", x, y, rest);
-            printf("firsttoken is %s\n", firsttoken);
-            printf("rest is %s\n", rest);
+            printf("firsttoken is %s\n", firsttoken); //TODO: DELETE
+            printf("rest is %s\n", rest); //TODO: DELETE
+
+            int n = strlen(rest), x, y;
+            rest[n] = '\n';
+            rest[n+1] = '\0'; //! lo cambie de '0' a '\0' TODO: OJOOOOO
+            if(sscanf(rest, "%d %d %[^\n]\n", &x, &y, rest) != 3)
+                printf("Error");
+            else{
+                printf("coordinate of the first point is (%d, %d), and the rest is %s", x, y, rest);
+
+                //* ADD CODE FOR HANDLING ADD
+
+            }
+        }
+        if(!strcmp(firsttoken, "shift")){
+            printf("SHIFT WAS NOT IMPLEMENTED\n");
+        }
+        if(!strcmp(firsttoken, "turn")){
+            printf("TURN WAS NOT IMPLEMENTED\n");
         }
     }
 
